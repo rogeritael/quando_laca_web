5851747f8b384b470f71b44923aaa7f349cf3ff9
Roger Rosa <rogeritael.contato@gmail.com>
2023-08-19 18:42:36 -0300

    funcionalidade do input de pesquisa

--- src/components/ui/Search/index.tsx
+++ src/components/ui/Search/index.tsx
@@ -2,15 +2,26 @@
 import Image from "next/image";
 import { SearchComponent } from "./styles";
 import SearchIcon from "@/assets/icons/Search.png";
+import { useEffect, useRef, useState } from "react";
+import Router from "next/router";
+
 
 interface SearchProps {
 
 }
 
 export function Search(props : SearchProps){
+    const [searchTerm, setSearchTerm] = useState('')
+
+    const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === "Enter") {
+          window.location.href = `/Search?term=${searchTerm}`;
+        }
+      };
+    
     return(
         <SearchComponent>
-            <input type="text" placeholder="encontre um jogo..." />
+            <input onKeyDown={(e) => handleKeyPress(e)} onChange={(e) => setSearchTerm(e.target.value)} type="text" placeholder="encontre um jogo..." />
             <Image
                 src={SearchIcon}
                 alt="search icon"
