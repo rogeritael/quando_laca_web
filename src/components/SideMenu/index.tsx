1ffdf79e5c0891b77877074bbf8d084be9a8bf9e
Roger Rosa <rogeritael.contato@gmail.com>
2023-08-23 12:55:56 -0300

    feedback de qual página está ativa no menu lateral

--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -1,26 +1,60 @@
 import { SideMenuContainer } from "./styles";
 import Image from 'next/image';
+import { useEffect, useState } from 'react'
 
 import categoryIcon from '@/assets/icons/menu_home.svg';
 import favoritedIcon from '@/assets/icons/menu_favorite.svg';
 import searchIcon from '@/assets/icons/menu_search.svg';
 import Link from "next/link";
+import { useRouter } from 'next/navigation';
 
 interface SideMenuProps {
 
 }
+
 export function SideMenu(props : SideMenuProps){
+    const [activeLink, setActiveLink] = useState<'home'|'library'|'search'|''>()
+
+    useEffect(() => {
+    
+        const fetchData = async() => {
+            let route = await window.location.href
+            route = route.split('/')[ route.split('/').length - 1]
+            route = route.split('?')[0]
+
+            switch(route){
+                case '':
+                    setActiveLink('home')
+                    break;
+                case 'Library':
+                    setActiveLink('library')
+                    break;
+                case 'Search':
+                    setActiveLink('search')
+                    break;
+                case 'About':
+                    setActiveLink('')
+                    break;
+                default:
+                    setActiveLink('home')
+                    break;
+            }
+        }
+        fetchData()
+
+    }, [])
+
     return(
         <SideMenuContainer className="side">
-                <a href="/" className="active">
-                    <Image src={categoryIcon} alt="ver todos os jogos" />
-                </a>
-                    <Link href="/Library">
-                    <Image src={favoritedIcon} alt="ver todos os jogos" />
-                    </Link>
-                <a href="/Search?term=populares">
-                    <Image src={searchIcon} alt="ver todos os jogos" />
-                </a>
+            <Link href="/" className={activeLink === 'home' ? 'active' : ''}>
+                <Image src={categoryIcon} alt="ver todos os jogos" />
+            </Link>
+            <Link href="/Library" className={activeLink === 'library' ? 'active' : ''}>
+                <Image src={favoritedIcon} alt="ver todos os jogos" />
+            </Link>
+            <Link href="/Search?term=populares" className={activeLink === 'search' ? 'active' : ''}>
+                <Image src={searchIcon} alt="ver todos os jogos" />
+            </Link>
         </SideMenuContainer>
     )
 }
