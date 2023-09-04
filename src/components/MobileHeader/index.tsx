4493f6f50bc8e144d23586a3f6eccaf16389444d
Roger Rosa <rogeritael.contato@gmail.com>
2023-09-03 21:19:34 -0300

    componente de search mobile

--- src/components/MobileHeader/index.tsx
+++ src/components/MobileHeader/index.tsx
@@ -3,18 +3,46 @@ import { Bell } from "../ui/bell";
 import { MobileHeaderContainer } from "./styles";
 import search from "@/assets/icons/Search.png"
 import { Logo } from "../ui/Logo";
+import SearchIcon from '@/assets/icons/Search.png'
+import { useState } from 'react';
 
 interface MobileHeaderProps {
 
 }
 
 export function MobileHeader(props : MobileHeaderProps){
+    const [ isInputVisible, setIsInputVisible ] = useState(false)
+    const [searchTerm, setSearchTerm] = useState('')
+
+    const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === "Enter") {
+            window.location.href = `/Search?term=${searchTerm}`;
+        }
+    };
+
+    function handleSearch(){
+        window.location.href = `/Search?term=${searchTerm}`;
+    }
 
     return(
         <MobileHeaderContainer className="header_mobile">
             <Logo />
-            <Image className="search" src={search} alt="icone de pesquisa de jogos" />
+            <Image className="search" src={search} alt="icone de pesquisa de jogos" onClick={() => setIsInputVisible(true)} />
             <Bell />
+            {
+                isInputVisible && (
+                    <div className="search_container">
+                        <Image
+                            src={SearchIcon}
+                            alt="search icon"
+                            onClick={() => handleSearch()}
+                        />
+                        <input type="text" placeholder="encontre um jogo.." value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+                        <span className="search_bg" onClick={() => setIsInputVisible(false)} />
+                    </div>
+                )
+            }
+            
         </MobileHeaderContainer>
     )
 }
--- src/components/MobileHeader/styles.ts
+++ src/components/MobileHeader/styles.ts
@@ -1,4 +1,16 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const fadeIn = keyframes`
+    to {
+        opacity: 0.7;
+    }
+`
+
+const slidefromTop = keyframes`
+    from {
+        top: -60px;
+    }
+`;
 
 export const MobileHeaderContainer = styled.header`
     width: 100%;
@@ -17,6 +29,60 @@ export const MobileHeaderContainer = styled.header`
 
     .bell {
         margin-bottom: -14px;
+    }
+
+    .search_container {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        
+        .search_bg {
+            width: 100%;
+            height: 100%;
+            display: inline-block;
+            position: fixed;
+            inset: 0;
+            background-color: var(--bg);
+            opacity: 0;
+            z-index: 1;
+        
+            animation: ${fadeIn} 400ms forwards;
+        }
+
+        img {
+            position: absolute;
+            right: 8px;
+            top: 18px;
+            z-index: 3;
+            animation: ${slidefromTop} 500ms forwards;
+        }
+
+        input {
+            width: 100%;
+            height: 60px;
+            position: absolute;
+            z-index: 2;
+            padding: 14px;
+            top: 0;
+            animation: ${slidefromTop} 500ms forwards;
+
+            background-color: var(--bg-secondary); //--bg-seconday
+            color: var(--highlight); //--highlight-color
+            font-size: 1rem;
+            transition: 200ms;
+
+            &:hover, &:focus {
+                background-color: var(--bg-secondary-lighten);
+            }
+
+            &::placeholder {
+                font-size: 0.875rem;
+                color: var(--highlight)
+            }
+        }
+
     }
 
     @media (max-width: 800px){
