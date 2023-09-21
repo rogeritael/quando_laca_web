540a7354170fa900e8c7e2b42eeda69d342edfcb
Roger Rosa <rogeritael.contato@gmail.com>
2023-09-20 23:17:21 -0300

    okay

--- src/components/ui/TrailerModal/index.tsx
+++ src/components/ui/TrailerModal/index.tsx
@@ -2,6 +2,7 @@ import { getVideoId } from "@/utils/getVideoId";
 import { ModalContainer } from "./styles";
 import { useEffect, useRef, useState } from 'react';
 import { AiOutlineCloseCircle } from "react-icons/ai";
+import { useRouter } from "next/navigation";
 
 interface TrailerModalProps {
     videoUrl: string;
@@ -12,6 +13,26 @@ interface TrailerModalProps {
 export function TrailerModal({ videoUrl, isOpen, setIsOpen } : TrailerModalProps){
     const embedUrl = `https://www.youtube.com/embed/${getVideoId(videoUrl)}`;
     const videoRef = useRef<HTMLIFrameElement | null>(null);
+    const router = useRouter();
+
+    useEffect(() => {
+        const handleBackButton = (e: Event) => {
+            // Verifica se o usuário pressionou o botão de voltar
+            if (e.type === 'popstate') {
+                // Faça o que você precisa quando o botão de voltar é pressionado
+                console.log('O botão de voltar foi pressionado');
+                setIsOpen(false)
+            }
+        };
+
+        // Adiciona um ouvinte de eventos ao histórico de navegação
+        window.addEventListener('popstate', handleBackButton);
+
+        return () => {
+            // Remove o ouvinte de eventos quando o componente é desmontado
+            window.removeEventListener('popstate', handleBackButton);
+        };
+    }, []);
 
 
     function handleCloseModal(){
